28eef2d8c099a4239467376660c0d2cf5cd4c62d
PaarthUttekar <84128253+Paarthuttekar@users.noreply.github.com>
2022-01-05 14:11:03 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jamesonsaunders.convertwebsitetoapp',
-  appName: 'Website to App',
+  appId: 'ga.paarth.LazycarrotStudios',
+  appName: 'Lazycarrot Studios',
   webDir: 'www',
   bundledWebRuntime: false,
 };
